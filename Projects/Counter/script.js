061603cbe50c3518a17a70bcd01860046a4efec2
rizzlybear15 <work.devaaryan@gmail.com>
2023-07-28 12:22:04 +0530

    New Project

---
+++ Projects/Counter/script.js
@@ -0,0 +1,22 @@
+const inc = document.getElementById("inc");
+const dec = document.getElementById("dec");
+const reset = document.getElementById("reset");
+const count = document.getElementById("count");
+
+let currCount = 0;
+
+inc.addEventListener("click", function () {
+  currCount = parseInt(count.innerHTML);
+  console.log(typeof(currCount));
+  count.innerHTML = currCount + 1;
+});
+
+dec.addEventListener("click", function () {
+  currCount = parseInt(count.innerHTML);
+  if(currCount>0)
+    count.innerHTML = currCount - 1;
+});
+
+reset.addEventListener("click", function () {
+  count.innerHTML = 0;
+});
